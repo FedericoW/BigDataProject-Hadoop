84aba084245a609ac5df628a02ae232e8d8e4e00
Federico Wang <fwang@cpp.edu>
2018-12-06 04:49:12 -0800

    Update Mapper1A.py

--- Program1/Mapper1A.py
+++ Program1/Mapper1A.py
@@ -6,6 +6,9 @@
 import sys
 import string
 
+#Mapper reading each line from standard input
+#Each line is a comma seperated value that has the form: business_id,stars
 for line in sys.stdin:
     line = line.strip()
+    #outputting a comma seperated value to standard output in the format: line, 1
     print "%s,%d" % (line, 1)
