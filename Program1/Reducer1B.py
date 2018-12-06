90111a407a2aee46708a0634dd7070eeb1861249
Federico Wang <fwang@cpp.edu>
2018-12-06 07:39:18 -0800

    Update Reducer1B.py

--- Program1/Reducer1B.py
+++ Program1/Reducer1B.py
@@ -11,11 +11,15 @@ m = 0
 curFreq = 0;
 bus_id = "random_id"
 
+#Reducer reads from standard input which is the output from each mapper runing the Mapper1B.py script which outputs the highest visited business in each spilt
+#i.e line = business_id, amount of ratings. It will output the highest visited business.
 for line in sys.stdin:
     cur_id, freq = line.split(",")
     curFreq = int(freq)
-    if curFreq>m:
+    #checks for the biggest amount fo ratings
+    #if the current amount is bigger than any previous amounts, replace the frequency number and bus_id    if curFreq>m:
         m= curFreq
         bus_id = cur_id
-      
+
+#write the business id along with its frequency into a output file
 print "%s,%d" % (bus_id, m)
